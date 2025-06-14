beaffa766ca1d20d0276bcc8b19b2d27273f8691
Crepalli <joao31105@edu.unipar.br>
2025-06-14 16:31:07 -0300

    Delete ExemploJavaScript directory

--- ExemploJavaScript/Lista 3/EX - 3 - Padaria/scirpt.js
+++
@@ -1,9 +0,0 @@
-let pao = document.querySelector("#pao");
-let broas = document.querySelector("#broas");
-let btcalcular = document.querySelector("#btcalcular");
-let h2resultado = document.querySelector("#h2resultado");
-
-function padaria (){
-    let paes = Number(pao.value);
-    let broa = Number(broas.value);
-}
